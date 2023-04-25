5c258bfe65ab1b3d3d3062cc0c38f55c39577f3a
MonkeytecMusic <l181190115@milpaalta2.tecnm.mx>
2023-04-25 11:00:41 -0600

    juego

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'juego_dados';
+    title = 'juego_dados';
+    public valor1:number = 0;
+    public valor2:number = 0;
+    public valor3:number = 0;
+    public resultado:string = "";
+
+    public numeroAleatorio():number{
+        return Math.trunc(Math.random()*6)+1;
+    }
+
+    constructor(){
+        this.valor1 = this.numeroAleatorio();
+        this.valor2 = this.numeroAleatorio();
+        this.valor3 = this.numeroAleatorio();
+    }
+
+    public tirar():void{
+        this.valor1 = this.numeroAleatorio();
+        this.valor2 = this.numeroAleatorio();
+        this.valor3 = this.numeroAleatorio();
+        if (this.valor1 == this.valor2 && this.valor1 == this.valor3) {
+            this.resultado = 'Gano';
+        }else{
+            this.resultado = 'Perdio';
+        }
+    }
 }
